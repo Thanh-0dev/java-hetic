164792a7ee3232a98046dd7b9759b3c08f0e5bda
ReaSlyn <reaslynn@gmail.com>
2024-04-11 12:26:19 +0200

    feat: functional programation

--- src/main/java/fr/hetic/FileProcessor.java
+++ src/main/java/fr/hetic/FileProcessor.java
@@ -1,44 +1,52 @@
 package fr.hetic;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.stream.Stream;
 
 public class FileProcessor {
     public void processFile(Path filePath) {
         Path outputPath = Paths.get(filePath.toString().replaceAll("\\.op$", ".res"));
-        try (BufferedReader reader = Files.newBufferedReader(filePath);
+        try (Stream<String> lines = Files.lines(filePath);
              BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String result = compute(line);
-                if (result != null) {
-                    writer.write(result);
-                    writer.newLine();
-                }
-            }
+            lines.map(line -> line.split(" "))
+                    .filter(parts -> parts.length == 3)
+                    .forEach(parts -> {
+                        performOperationToFile(parts, writer);
+                    });
         } catch (IOException e) {
             System.err.println("Error processing file: " + e.getMessage());
         }
     }
 
-    private String compute(String inputLine) {
-        String[] parts = inputLine.split(" ");
-        if (parts.length != 3) {
-            return "ERROR";
-        }
-
+    private static boolean isValidNumber(String str) {
         try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static void performOperationToFile(String[] parts, BufferedWriter writer) {
+        try {
+            if (!isValidNumber(parts[0]) || !isValidNumber(parts[1])) {
+                writer.write("ERROR");
+                writer.newLine();
+                return;
+            }
+
             int num1 = Integer.parseInt(parts[0]);
             int num2 = Integer.parseInt(parts[1]);
             String operator = parts[2];
-
             OperationStrategy strategy = OperationStrategyFactory.getStrategy(operator);
-            return (strategy != null) ? strategy.execute(num1, num2) : "ERROR";
-        } catch (NumberFormatException e) {
-            return "ERROR";
+
+            writer.write((strategy != null) ? strategy.execute(num1, num2) : "ERROR");
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error processing file: " + e.getMessage());
         }
     }
 }
